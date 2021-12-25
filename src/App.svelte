3b1162f57249e644303faa662c1b5dc5653a7b09
Floyd Jones <floydtjones@gmail.com>
2021-12-25 13:54:27 -0600

    Add toolbar, footer, dashboard. refactor

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,10 @@
 <script lang="ts">
-  import ArticleCard from "./ArticleCard.svelte";
   import LoginModal from "./LoginModal.svelte";
   import Nav from "./Nav.svelte";
   import { whichModalIsOpen } from "./stores/modal";
   import { user } from "./stores/user";
-  import Fa from "svelte-fa";
-  import { faGithub } from "@fortawesome/free-brands-svg-icons";
+  import Dashboard from "./Dashboard.svelte";
+  import Footer from "./Footer.svelte";
 
   $: console.log($user);
 
@@ -16,27 +15,11 @@
 
 <main>
   <Nav />
-  <div class="articles">
-    <ArticleCard />
-    <ArticleCard />
-    <ArticleCard />
-    <ArticleCard />
-    <ArticleCard />
-    <ArticleCard />
-    <ArticleCard />
-    <ArticleCard />
-    <ArticleCard />
-    <ArticleCard />
-  </div>
+  <Dashboard />
+  <Footer />
   {#if $whichModalIsOpen === "login"}
     <LoginModal />
   {/if}
-  <div class="footer">
-    <div>Created with ❤️ by Floyd Jones</div>
-    <a target="__blank" href="https://github.com/flooyd/svelte-blog"
-      ><Fa icon={faGithub} /></a
-    >
-  </div>
 </main>
 
 <style>
@@ -46,31 +29,5 @@
     font-family: "Josefin Sans", Arial, Helvetica, sans-serif;
     background-color: whitesmoke;
     padding-bottom: 20px;
-  }
-
-  .articles {
-    margin: 20px 0px;
-    display: grid;
-    grid-template-columns: repeat(auto-fill, minmax(350px, 600px));
-    justify-content: center;
-    grid-gap: 20px;
-  }
-
-  .footer {
-    position: fixed;
-    background: #333;
-    color: white;
-    width: 100%;
-    bottom: 0px;
-    text-align: right;
-    padding: 10px;
-    padding-right: 20px;
-  }
-
-  .footer a,
-  .footer div {
-    color: white;
-    display: inline-block;
-    margin-left: 13px;
   }
 </style>
